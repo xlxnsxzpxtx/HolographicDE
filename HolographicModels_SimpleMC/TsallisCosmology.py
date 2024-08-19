6649cc61737aa54c8316d058e91f909b3e2a4290
= <=>
2024-08-19 11:24:55 -0600

    Tsallis EoS

--- HolographicModels_SimpleMC/TsallisCosmology.py
+++ HolographicModels_SimpleMC/TsallisCosmology.py
@@ -1,43 +1,37 @@
-from simplemc.models.LCDMCosmology import LCDMCosmology
-from simplemc.cosmo.Parameter import Parameter
-from simplemc.cosmo.paramDefs import Ok_par
+import numpy as np
 from scipy.interpolate import interp1d
 from scipy.integrate import odeint
-from scipy import optimize
-import numpy as np
-
-
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from simplemc.models.LCDMCosmology import LCDMCosmology
+from simplemc.cosmo.Parameter import Parameter
 
 class TsallisCosmology(LCDMCosmology):
     """
-        This is Holographic Dark Energy in modfied Tsallis Cosmology.
-        This class inherits LCDMCosmology class as the rest of the cosmological
-        models already included in SimpleMC.
-
-        :param varyc: variable w0 parameter
-
+    This is Holographic Dark Energy in modified Tsallis Cosmology.
+    This class inherits LCDMCosmology class as the rest of the cosmological
+    models already included in SimpleMC.
+    :param varyc: variable w0 parameter
     """
 
- 
     def __init__(self, varyc=True,varys=True):
-        # Holographic and Tsallis parameter
-        self.c_par = Parameter("c", 1.0, 0.05, (0.2, 2.0), "c")
-        self.s_par = Parameter("s", 0.5, 0.05, (0.0, 3.0), "s") 
-        #self.B_par = Parameter("B", 3.0, 0.05, (2.5,3.5), "B") # https://arxiv.org/abs/1806.01301
+        # Holographic and Barrow parameter
+        self.c_par = Parameter("c", 0.5, 0.01, (0.0,2.0), "c")
+        self.s_par = Parameter("s", 1.5, 0.01, (1.0,1.5), "s") 
         #self.varyOk = varyOk
         self.varyc  = varyc
         self.varys  = varys  
-        #self.varyB  = varyB
         
-        self.Ok = Ok_par.value
+        #self.Ok = Ok_par.value
         self.c  = self.c_par.value # holographic parameter 
-        self.s  = self.s_par.value # Tsallis parameter
-        #self.B =  self.B_par.value 
+        self.s  = self.s_par.value # barrow parameter
+
         # This value is quite related to the initial z
         self.zini = 3
-        self.xini = np.log(1./(1+self.zini))
+        #self.xfin = np.log(1./(1+self.zini))
         #self.scale = 10**(-2)
-        self.xvals = np.linspace(0,self.xini,50)
+        self.zvals = (0, 3)
+        self.t_eval = np.linspace(0, self.zini, 50)
 
         LCDMCosmology.__init__(self)
         self.updateParams([])
@@ -49,7 +43,6 @@ class TsallisCosmology(LCDMCosmology):
         #if (self.varyOk): l.append(Ok_par)
         if (self.varyc):  l.append(self.c_par)
         if (self.varys):  l.append(self.s_par)
-        #if (self.varyB):  l.append(self.B_par)
         return l
 
 
@@ -59,63 +52,87 @@ class TsallisCosmology(LCDMCosmology):
             return False
         for p in pars:
             if p.name   == "c":
-              self.c = p.value
+                self.c = p.value
             elif p.name == "s":
-              self.s = p.value
-            #elif p.name == "B": 
-            #    self.B == p.value
-            #elif p.name == "Ok":
-             #   self.Ok = p.value
-            #self.setCurvature(self.Ok)
-            #if (abs(self.Ok) > 1.0):
-             #    return False
+                self.s = p.value
+
 
         self.initialize()
         return True
 
 # x = ln(a)
-    def RHS_x_hde_tsallis(self, Omega, x):    
-        b = 2*(self.s -1)
-        Q = (2-Omega)*((self.c**2)**(1./(b - 2)))*((self.h*np.sqrt(self.Om))**(b/(2 - b)))
-        dOmega = Omega*(1-Omega)*(b + 1 + Q*((1-Omega)**(b/2*(b -2)))*(Omega**(1./(2-b)))*np.exp((3*b/(2-b))*x))
+    
+
+
+
+
+
+
+    def RHS_hde(self,z,Omega):
+        exponent1 = (1-self.s)/(2*(2-self.s))
+        exponent2 = 1/(2*(2-self.s))
+        exponent3 = 3*(1-self.s)/(2*(2-self.s))
+        exponent4 = (1-self.s)/(self.s-2) 
+        exponent5 = 1/(2*(self.s-2))
+
+        x = np.log(1./(1+z))
+        H0 = (100*self.h)
+        Q =2*(2-self.s)*((self.c*2)**exponent5)*((H0*np.sqrt(self.Om))**exponent4)
+        # Compute common terms
+            
+        
+        
+        factor1 = - (Omega*(1 - Omega))/(1+z)
+        term1 = 2*self.s-1 
+        term2 = Q*((1-Omega)**exponent1)*(Omega**exponent2)*np.exp(exponent3*x)
+
+        dOmega = factor1*(term1 + term2)
+
         return dOmega
 
-   # def compute_Ode(self, Ode_ini):
-    #    Ode = Ode_ini*self.scale
-    #    solution = odeint(self.RHS_a_hde_tsallis, Ode, self.avals, h0=1E-5)
-     #   return solution
+    def EoS(self,z,Omega):
+        
+        
+        exponent1= 1/(2*(2-self.s))
+        exponent2 = (self.s-1)/(2*(self.s-2))
+        exponent3 = 3*(1-self.s)/(2*(self.s-2))
+        exponent4 = (1-self.s)/(self.s-2) 
+        exponent5 = 1/(2*(self.s-2))
+        
+        x = np.log(1./(1+z))
+        H0 = (100*self.h)
+        Q =2*(2-self.s)*((self.c*2)**exponent5)*((H0*np.sqrt(self.Om))**exponent4)
 
 
-    #def ini_sol(self, Ode_ini):
-     #   diference = self.compute_Ode(Ode_ini)[-1] - (1-self.Om-self.Ok)
-      #  return diference
+        w = (1-2*self.s)/3 - (Q/3)*(Omega**exponent1)*((1-Omega)**exponent2)*np.exp(exponent3*x)
+        
+        return w 
+
+    
+
 
 
     def initialize(self):
-        """
-        Main method that searches the initial conditions for a given model.
-
-        """
-    
-        if 1.9999<self.s<2.001: 
-           Ode0 = (1 - self.Om)   
-        else: 
-            Ode0 = 1 - self.Om
-            result_E = odeint(self.RHS_x_hde_tsallis, Ode0, self.xvals)
-
-            self.Ode = interp1d(self.xvals, result_E[:, 0])
-           #print(self.s)
+        
+        Ode0 = [1 - self.Om]
+        result_E = solve_ivp(self.RHS_hde, self.zvals, Ode0, t_eval=self.t_eval, method='RK45', atol=1e-12, rtol=1e-12)
+        
+        # Interpolate the result
+        self.Ode = interp1d(result_E.t, result_E.y[0], kind='cubic')
+        #z_plot = np.linspace(0, self.zini, 50)
+        #Omega_values = self.Ode(z_plot)
+        #Eos_values = self.EoS(z_plot, Omega_values)
+        
+      
+        #plt.plot(z_plot, Eos_values)
+        #result_E_re = result_E.reshape(-1)
+        #Eos_linear = self.EoS(result_E_re,self.zvals)
+        #print(result_E)
+        #plt.plot(self.zvals,result_E_re)
+        #plt.grid(True)
+        #plt.xlabel('z')
+        #plt.ylabel('$\omega_{de}$')
         return True
-
-        #ini_val = optimize.newton(self.ini_sol, 1)
-        
-        #Ode = self.compute_Ode(ini_val)
-
-        #ini_vals = Ode0
-    
-
-         #found, i.e. c<0.4
-        
 
 
 
@@ -123,8 +140,11 @@ class TsallisCosmology(LCDMCosmology):
     # this is relative hsquared as a function of a
     ## i.e. H(z)^2/H(z=0)^2
     def RHSquared_a(self, a):
-        x = np.log(a)
-        hubble = (self.Om/a**3)/(1-self.Ode(x))
+        z = 1./a-1
+        hubble = (self.Om/a**3)/(1-self.Ode(z))
+
+        #print(100*self.h*hubble)
+        #print(self.b)
+        #f1 = (self.Om/a[0]**3)/(1-self.Ode(z)[0])
+        #print(100*self.h*hubble)      
         return hubble
-
-
