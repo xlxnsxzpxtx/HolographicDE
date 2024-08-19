82a44dae32f91ef362e033b60794a4a35649748f
= <=>
2024-08-19 11:20:01 -0600

    EoS

--- HolographicModels_SimpleMC/LinearCosmology.py
+++ HolographicModels_SimpleMC/LinearCosmology.py
@@ -2,12 +2,9 @@ from simplemc.models.LCDMCosmology import LCDMCosmology
 from simplemc.cosmo.Parameter import Parameter
 from simplemc.cosmo.paramDefs import Ok_par
 from scipy.interpolate import interp1d
-from scipy.integrate import odeint
-from scipy import optimize
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import numpy as np
-
-
 
 class LinearCosmology(LCDMCosmology):
     """
@@ -16,42 +13,36 @@ class LinearCosmology(LCDMCosmology):
         models already included in SimpleMC.
 
         :param varyc: variable w0 parameter
-
     """
 
-
-    def __init__(self,varyc = True,varyb = True,varya = True ):
+    def __init__(self, varyc=True, varyb=True, varya=True):
         # Holographic parameter
-        self.c_par = Parameter("c",1.0, 0.1, (0.5, 2.0), "c")
-        self.b_par = Parameter("b",0.5, 0.01,(0.0,1.0),  "b")
-        self.a_par = Parameter("a",0.8, 0.01, (0.0,1.0), "a")
-        self.varyc  = varyc
-        self.varyb  = varyb
-        self.varya  = varya 
+        self.c_par = Parameter("c",   1.0,    0.001,  (0.0, 2), "c")
+        self.b_par = Parameter("b",   0.25,  0.001,   (-0.5,0.5), "b")
+        self.a_par = Parameter("a",   0.25,  0.001,   (-0.5,0.5), "a")
+        self.varyc = varyc
+        self.varyb = varyb
+        self.varya = varya
 
-        self.c  = self.c_par.value
-        self.b  = self.b_par.value 
-        self.a  = self.a_par.value 
+        self.c = self.c_par.value
+        self.b = self.b_par.value
+        self.a = self.a_par.value
 
-              # This value is quite related to the initial z
+        # This value is quite related to the initial z
         self.zini = 3
-        #self.avals = np.linspace(1./(1+self.zini), 1, 300)
-        self.zvals    = np.linspace(0, self.zini, 50)
-        #self.z_int = np.linspace(0, 2, 500)
+        self.zvals = (0, 3)
+        self.t_eval = np.linspace(0, self.zini, 50)
 
         LCDMCosmology.__init__(self)
         self.updateParams([])
 
-
     # my free parameters. We add Ok on top of LCDM ones (we inherit LCDM)
     def freeParameters(self):
         l = LCDMCosmology.freeParameters(self)
-        #if (self.varyOk): l.append(Ok_par)
-        if (self.varyc):  l.append(self.c_par)
-        if (self.varyb):  l.append(self.b_par)
-        if (self.varya):  l.append(self.a_par)
+        if self.varyc: l.append(self.c_par)
+        if self.varyb: l.append(self.b_par)
+        if self.varya: l.append(self.a_par)
         return l
-
 
     def updateParams(self, pars):
         ok = LCDMCosmology.updateParams(self, pars)
@@ -59,54 +50,56 @@ class LinearCosmology(LCDMCosmology):
             return False
         for p in pars:
             if p.name == "c":
-                self.c = p.value  
+                self.c = p.value
             elif p.name == "b":
-               self.b = p.value
+                self.b = p.value
             elif p.name == "a":
-               self.a = p.value 
-            
-
+                self.a = p.value
         self.initialize()
         return True
 
+    def RHS_hde(self, z, Omega):
+        x = np.log(1. / (1 + z))
+        H0 = (100*self.h)
+        exponent1 = (self.a + self.b*z) / (2 * (self.a + self.b*z - 2))
+        exponent2 = -(1+z)*self.b/(self.a +self.b*z-2)
+        Q = ((self.c**2)* np.exp(3*x)) / ((H0**2)*self.Om)
+        dOmega_DE_dz =  -((Omega*(1 - Omega))/ (1 + z))*(np.sqrt(Omega)*((self.c**2)**(-0.5))*((Q*(1-Omega)/Omega)**exponent1)*(2-self.a-self.b*z) + np.log((Q*(1-Omega)/Omega)**exponent2) + self.a + self.b*z +  1)
+        return dOmega_DE_dz
 
-    def RHS_hde(self, Omega, z):
-        f = self.a + self.b*z
-        f_prim =  self.b
-        Q = self.c**2/((self.Om*(1+z)**3)*(self.h**2))
-        dOmega = - (Omega*(1-Omega)/(1+z))*((2*np.sqrt(Omega)/self.c)*((Q*(1-Omega)/Omega)**((f-1)/(2*(f-2))))*(2 - f)  + 2*f -  2 + (np.log(Q*(1-Omega)/Omega)**(-(1+z)*f_prim/(f-2))) )
-        return dOmega
-
-
-
+    def EoS(self,z,Omega):
+        x = np.log(1. / (1 + z))
+        H0 = (100*self.h)
+        Q = ((self.c ** 2) * np.exp(3 * x)) / ((H0 ** 2) * self.Om)
+        exponent1 = (self.a + self.b * z) / (2 * (self.a + self.b * z - 2))
+        exponent2 = 1. / (2 - self.a - self.b * z)
+        w_DE = -(self.a + self.b * z + 1) / 3 + ((self.a + self.b * z - 2) * np.sqrt(Omega) / 3)*((self.c ** 2) ** (-0.5))*((Q*(1 - Omega)/Omega)**exponent1) + ((1 + z) * self.b / 3) * np.log((Q * (1 - Omega) / Omega) ** exponent2)
+        return w_DE
 
     def initialize(self):
+        Ode0 = [1 - self.Om]
+        result_E = solve_ivp(self.RHS_hde, self.zvals, Ode0, t_eval=self.t_eval, method='RK45', atol=1e-12, rtol=1e-12)
         
-        Ode0 = (1 - self.Om)
-        result_E = odeint(self.RHS_hde, Ode0, self.zvals)
-        self.Ode = interp1d(self.zvals, result_E[:,0])
-        #print(result_E[:,0])
-        #f = self.Ode(self.z_int)
-        #print('Omega_de(z=0) para cada valor usando interp1d',f[0])
-        #self.Omega_hde = interp1d(self.avals, Ode[:, 0])
+        # Interpolate the result
+        self.Ode = interp1d(result_E.t, result_E.y[0], kind='cubic')
+        #print("Solution values (y[0]):", result_E.y[0])
+        # Plot the EoS for demonstration purposes
+        
+        #z_plot = np.linspace(0, self.zini, 50)
+        #Omega_values = self.Ode(z_plot)
+        #Eos_values = self.EoS(z_plot, Omega_values)
+        
+       
+        #plt.plot(z_plot, Eos_values)
+        #plt.grid(True)
+        #plt.xlabel('z')
+        #plt.ylabel('$\omega_{de}$')
+        #plt.legend()
+        #plt.show()
+
         return True
 
-    
-
-    # this is relative hsquared as a function of a
-    ## i.e. H(z)^2/H(z=0)^2
     def RHSquared_a(self, a):
-        z = 1./a-1
-        hubble = (self.Om/a**3)/(1-self.Ode(z))
-        #print(100*self.h*hubble)
-        #print(self.b)
-        #f1 = (self.Om/a[0]**3)/(1-self.Ode(z)[0])
-        #print(100*self.h*hubble)      
+        z = 1. / a - 1
+        hubble = (self.Om / a ** 3) / (1 - self.Ode(z))
         return hubble
-
-
-    
-
-    
-
-
