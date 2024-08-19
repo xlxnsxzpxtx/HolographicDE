5327e366dc9d60e7d9182ed644fbd693b72c13aa
= <=>
2024-08-19 11:18:48 -0600

    EoS

--- HolographicModels_SimpleMC/BarrowHDECosmology.py
+++ HolographicModels_SimpleMC/BarrowHDECosmology.py
@@ -1,9 +1,11 @@
+# Barrow cosmology
 from simplemc.models.LCDMCosmology import LCDMCosmology
 from simplemc.cosmo.Parameter import Parameter
 from simplemc.cosmo.paramDefs import Ok_par
 from scipy.interpolate import interp1d
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 from scipy import optimize
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -21,9 +23,9 @@ class BarrowHDECosmology(LCDMCosmology):
 
     def __init__(self, varyc=True,varyb=True):
         # Holographic and Barrow parameter
-        self.c_par = Parameter("c", 0.8, 0.05, (0.5,2.0), "c")
-        self.b_par = Parameter("b", 1.5, 0.05, (0.0,3.0),  "b") 
-        #self.varyOk = varyOk
+        self.c_par = Parameter("c", 1.5, 0.0001, (1.0,2.0), "c")
+        self.b_par = Parameter("b", 0.2, 0.000001, (0.1,0.4), "b") 
+        #self.varyOk = varyOkq
         self.varyc  = varyc
         self.varyb  = varyb  
         
@@ -33,9 +35,10 @@ class BarrowHDECosmology(LCDMCosmology):
 
         # This value is quite related to the initial z
         self.zini = 3
-        self.xfin = np.log(1./(1+self.zini))
+        #self.xfin = np.log(1./(1+self.zini))
         #self.scale = 10**(-2)
-        self.xvals = np.linspace(0,self.xfin, 50)
+        self.zvals = (0, 3)
+        self.t_eval = np.linspace(0, self.zini, 50)
 
         LCDMCosmology.__init__(self)
         self.updateParams([])
@@ -59,49 +62,76 @@ class BarrowHDECosmology(LCDMCosmology):
                 self.c = p.value
             elif p.name == "b":
                 self.b = p.value
-            #elif p.name == "Ok":
-            #      self.Ok = p.value
-            #      self.setCurvature(self.Ok)
-            #      if (abs(self.Ok) > 1.0):
-            #          return False
+
 
         self.initialize()
         return True
 
 # x = ln(a)
-    def RHS_x_hde_barrow(self, vals,x):
-        Omega = vals 
-        Q = (2-Omega)*((self.c**2)**(1./(self.b-2)))*((self.h*np.sqrt(self.Om))**(self.b/(2 - self.b)))
-        dOmega = Omega*(1-Omega)*(self.b + 1 + Q*((1-Omega)**(self.b/2*(self.b -2)))*(Omega**(1./(2-self.b)))*np.exp((3*self.b/(2-self.b))*x))
-        #print(self.c)
+    
+
+
+
+
+
+
+    def RHS_hde(self,z,Omega):
+        x = np.log(1./(1+z))
+        H0 = 100*self.h
+
+        exp1 = 1/(self.b-2)
+        exp2 = self.b/(2-self.b)
+        exp3 = self.b/(2*(self.b-2))     
+        exp4 = 1/(2-self.b)
+        exp5 = 3*self.b/(2*(self.b-2))
+
+        Q = (2-self.b)*((self.c**2)**exp1 )*((H0*np.sqrt(self.Om))**exp2)
+
+        
+        factor1 = - (Omega*(1 - Omega))/(1+z) 
+        term1 = self.b + 1 
+        term2 = Q*((1-Omega)**exp3)*(Omega**exp4)*np.exp(exp5*x)
+
+        dOmega = factor1*(term1 + term2)
+        
+
+
+    
         return dOmega
 
+    def EoS(self,z,Omega):
+        
+        
+        exponent1= 1/(2-self.b)
+        exponent2 = self.b/(2*(self.b-2))
+        exponent3 = 3*self.b/(2*(2-self.b))
+        exponent4 = (self.b)/(2-self.b) 
+        exponent5 = 1/(self.b-2)
+        
+        x = np.log(1./(1+z))
+        H0 = (100*self.h)
+        Q =(2-self.b)*((self.c*2)**exponent5)*((H0*np.sqrt(self.Om))**exponent4)
 
-    #$def compute_Ode(self, Ode_ini):
-      #  Ode = Ode_ini*self.scale
-       # solution = odeint(self.RHS_a_hde_barrow, Ode, self.avals, h0=1E-5)
-        #return solution
 
+        w = -(1+self.b)/3 - (Q/3)*(Omega**exponent1)*((1-Omega)**exponent2)*np.exp(exponent3*x)
+        
+        return w 
 
-    #def ini_sol(self, Ode_ini):
-     #   diference = self.compute_Ode(Ode_ini)[-1] - (1-self.Om-self.Ok)
-      #  return diference
 
 
     def initialize(self):
-        """
-        Main method that searches the initial conditions for a given model.
-        """
-             #ini_val = optimize.newton(self.ini_sol, 1)
-        Ode0 = (1 - self.Om)
-        #Ode = self.compute_Ode(ini_val)
-
-        #ini_vals = Ode0
-        result_E = odeint(self.RHS_x_hde_barrow, Ode0, self.xvals)
-        #Ode = np.exp(result_E[:, 0])
-        self.Ode = interp1d(self.xvals, result_E[:, 0])
-
-        ## Add a flag in case the ini condition isn't found, i.e. c<0.4
+        
+        Ode0 = [1 - self.Om]
+        result_E = solve_ivp(self.RHS_hde, self.zvals, Ode0, t_eval=self.t_eval, method='RK45', atol=1e-12, rtol=1e-12)
+        
+        # Interpolate the result
+        self.Ode = interp1d(result_E.t, result_E.y[0], kind='cubic')
+        z_plot = np.linspace(0, self.zini, 50)
+        Omega_values = self.Ode(z_plot)
+        Eos_values = self.EoS(z_plot, Omega_values)
+        
+        #plt.plot(z_plot, Eos_values)
+      
         return True
 
 
@@ -110,7 +140,11 @@ class BarrowHDECosmology(LCDMCosmology):
     # this is relative hsquared as a function of a
     ## i.e. H(z)^2/H(z=0)^2
     def RHSquared_a(self, a):
-        x = np.log(a)
-        hubble = (self.Om/a**3)/(1-self.Ode(x))
-        return hubble
+        z = 1/a-1
+        hubble = (self.Om/a**3)/(1-self.Ode(z))
 
+        #print(100*self.h*hubble)
+        #print(self.b)
+        #f1 = (self.Om/a[0]**3)/(1-self.Ode(z)[0])
+        #print(100*self.h*hubble)      
+        return hubble
